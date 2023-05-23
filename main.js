ab115fd575a04128773eebff6db7f7c763a5a8e7
Zuhayr Amin <zuhayr@Zuhayrs-MacBook-Pro.local>
2023-05-23 20:32:04 +0500

    Almost done
    The app is functioning as desired. It gets user location and displays in depth information about the current day, gives an overview of the week and gives detailed hourly info for the whole week as well. The only thing that needs to be updated in the icon colors, from black to a darker green which matches the theme.

--- main.js
+++ main.js
@@ -1,6 +1,88 @@
-
 import { getWeather } from "./weather.js"
+import { ICON_MAP } from "./iconMap.js"
 
-getWeather(10,10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(res => {
-    console.log(res.data)
-})
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+
+function positionSuccess({coords}){
+    getWeather(coords.latitude,
+        coords.longitude,
+        Intl.DateTimeFormat().resolvedOptions().timeZone
+        )
+
+    .then(renderWeather)
+    .catch( e => {
+        console.log(e)
+        console.log("Error getting weather data")
+    })
+}
+
+function positionError(){
+    alert(
+        "There was an error getting your location. Please allow us to use your location and refresh the page"
+    )
+}
+
+function renderWeather({current, daily, hourly}){
+    renderCurrentWeather(current),
+    renderDailyWeather(daily),
+    renderHourlyWeather(hourly),
+    document.body.classList.remove('blurred')
+}
+
+function setValue(selector, value, {parent = document} = {}){
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+const currentIcon = document.querySelector('[data-current-icon]')
+
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.png`
+}
+
+function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
+
+}
+
+const dailySection = document.querySelector("[data-day-section]")
+const dayCardTemplate = document.getElementById("day-card-template")
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, {weekday: "long"})
+
+function renderDailyWeather(daily) {
+    dailySection.innerHTML = ""
+
+    daily.forEach(day => {
+        const element = dayCardTemplate.content.cloneNode(true)
+        setValue("temp", day.maxTemp, {parent: element})
+        setValue("date", DAY_FORMATTER.format(day.timestamp), {parent: element})
+        element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
+        dailySection.append(element)
+    })
+}
+
+
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, {hour: "numeric"})
+const hourlySection = document.querySelector("[data-hour-section]")
+const hourlyRowTemplate = document.getElementById('hour-row-template')
+function renderHourlyWeather(hourly) {
+    hourlySection.innerHTML = ""
+
+    hourly.forEach(hour => {
+        const element = hourlyRowTemplate.content.cloneNode(true)
+        setValue("temp", hour.temp, {parent: element})
+        setValue("fl-temp", hour.feelsLike, {parent: element})
+        setValue("wind", hour.windSpeed, {parent: element})
+        setValue("precip", hour.precip, {parent: element})
+        setValue("day", DAY_FORMATTER.format(hour.timestamp), {parent: element})
+        setValue("time", HOUR_FORMATTER.format(hour.timestamp), {parent: element})
+        element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode)
+        hourlySection.append(element)
+    })
+}
--- weather.js
+++ weather.js
@@ -9,5 +9,67 @@ export function getWeather(lat, lon, timezone) {
         timezone,
         },
     })
-)
+).then(({data}) => {
+    return {
+        current: parseCurrentWeather(data),
+        daily: parseDailyWeather(data),
+        hourly: parseHourlyWeather(data)
+    }
+})
+}
+
+function parseCurrentWeather({current_weather, daily}){
+    const {
+        temperature: currentTemp,
+        windspeed: windSpeed,
+        weathercode: iconCode
+    } = current_weather
+
+    const {
+        // This is the same as saying const maxTemp = daily.temperature_2m_max[0], where you get the first value in the array
+        temperature_2m_max: [maxTemp],
+        temperature_2m_min: [minTemp],
+        apparent_temperature_max: [maxFeelsLike],
+        apparent_temperature_min: [minFeelsLike],
+        precipitation_sum: [precip],
+    } = daily
+
+    return {
+        currentTemp: Math.round(currentTemp),
+        highTemp: Math.round(maxTemp),
+        lowTemp: Math.round(minTemp),
+        highFeelsLike: Math.round(maxFeelsLike),
+        lowFeelsLike: Math.round(minFeelsLike),
+        windSpeed: Math.round(windSpeed),
+        // Rounding precip to the nearest 100th
+        precip: Math.round(precip * 100) / 100,
+        iconCode
+    }
+
+}
+
+function parseDailyWeather({daily}) {
+    return daily.time.map((time, index) => {
+        return {
+            // API returns the time in seconds, JS expects time in ms, thats why I multiplied by 1000
+            timestamp: time * 1000,
+            iconCode: daily.weathercode[index],
+            maxTemp: Math.round(daily.temperature_2m_max[index])
+        }
+    })
+}
+
+function parseHourlyWeather({hourly, current_weather}) { 
+    return hourly.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: hourly.weathercode[index],
+            temp: Math.round(hourly.temperature_2m[index]),
+            feelsLike: Math.round(hourly.apparent_temperature[index]),
+            windSpeed: Math.round(hourly.windspeed_10m[index]),
+            precip: Math.round(hourly.precipitation[index] * 100) / 100,
+        }
+    }).filter(({timestamp}) => timestamp >= current_weather.time * 1000)
+    // the filter ensures that we only display weather data for after the current time
+    // there's no point displaying weather info for the hours of the day that have already passed
 }
